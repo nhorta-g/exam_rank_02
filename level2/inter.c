78150b38a8f619b6a7503d46d1c7097a662ca387
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2022-11-07 19:44:58 +0000

    Add files via upload

--- level2/inter.c
+++ level2/inter.c
@@ -1,14 +1,28 @@
 #include <unistd.h>
 
-int	inter(av1, av2)
+size_t	ft_strlen(char *str)
 {
-	
+	size_t	len;
+
+	len = 0;
+	while(*(str++))
+		len++;
 }
 
 int	main(int ac, char **av)
 {
+	char	*str;
+	size_t	max_len;
+
+	max_len = 0;
 	if (ac == 2)
 	{
+		if (ft_strlen(av[1]) >= ft_strlen(av[2]))
+			max_len = ft_strlen(av[1]);
+		else
+			max_len = ft_strlen(av[2]);
+		str = (char *)malloc(max_len * sizeof char + 1);
+
 
 	}
 	else
